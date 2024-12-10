a30cec0963ec9245b27e822b219992193aa6b17c
Hakaabi <hakaabi@andrew.cmu.edu>
2024-12-10 17:25:00 +0300

    added if statement taken from api/controllers/game/draw.js

--- api/helpers/game-logic/validateDrawConditions.js
+++ api/helpers/game-logic/validateDrawConditions.js
@@ -1,2 +1,12 @@
 module.exports = function validateDrawConditions(game, playerId, playerTurn) {
+  if (game.oneOff) {
+    throw new Error("Can't play while waiting for opponent to counter");
+  }
+  if (playerId !== playerTurn % 2) {
+    throw new Error('game.snackbar.global.notYourTurn');
+  }
+  if (!game.topCard) {
+    throw new Error('game.snackbar.draw.deckIsEmpty');
+  }
+  return game;
 };
