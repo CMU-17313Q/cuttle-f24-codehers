a89a2e58b0e7b15dbb3caf14c4e6f7622a704f25
Ruba Al Mahmoud <Rubaalmahmoud@MacBook-Air-9.local>
2024-12-10 12:52:52 +0300

    Update create.js to pass player0Id and player1Id when creating a game

--- api/controllers/game/create.js
+++ api/controllers/game/create.js
@@ -1,22 +1,25 @@
 const gameAPI = sails.hooks['customgamehook'];
 
 module.exports = function (req, res) {
-  const { gameName, isRanked = false } = req.body;
+  const { gameName, isRanked = false, player0Id, player1Id } = req.body;
+
   if (req.body.gameName) {
     gameAPI
-      .createGame(gameName, isRanked)
+      .createGame(gameName, isRanked, gameService.GameStatus.CREATED, player0Id, player1Id)
       .then(function (game) {
         sails.sockets.broadcast('GameList', 'gameCreated', {
           id: game.id,
           name: game.name,
           status: game.status,
           isRanked: game.isRanked,
-          players: [],
+          players: [], // This will now be populated based on p0 and p1
         });
         return res.ok({ gameId: game.id });
       })
       .catch(function (reason) {
         res.badRequest(reason);
       });
+  } else {
+    res.badRequest({ message: "Game name is required" });
   }
 };
