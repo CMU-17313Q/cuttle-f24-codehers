2ece100f31fc88c1bdaa93e06b460ffc049b0892
Hakaabi <hakaabi@andrew.cmu.edu>
2024-12-10 20:36:34 +0300

    updated my publishGameState.js

--- api/helpers/game-logic/publishGameState.js
+++ api/helpers/game-logic/publishGameState.js
@@ -1,4 +1,9 @@
 module.exports = function publishGameState(game) {
+  if (!game) {
+    console.error('Game object is missing');
+    return;
+  }
+
   Game.publish([game.id], {
     change: 'draw',
     game,
