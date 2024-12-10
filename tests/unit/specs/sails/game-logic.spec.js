047344a1986f652d66f7a8606873932e4ca16763
Hakaabi <hakaabi@andrew.cmu.edu>
2024-12-10 18:15:06 +0300

    created a test file for the helper logic folder i created following the way game-state.js folder is being tested

---
+++ tests/unit/specs/sails/game-logic.spec.js
@@ -0,0 +1,4 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import draw from '../../../api/controllers/game/draw';
+import * as gameService from '../../../api/services/gameService';
+import * as userService from '../../../api/services/userService';
